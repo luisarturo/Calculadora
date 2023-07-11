e00e1cc21bb971d915c0b3b0e3be50a1c706af55
Luis Arturo Cardenas Bustamante <luis.cardenas.bustamante@gmail.com>
2023-07-11 11:34:53 -0700

    Modificación que se realizo en el proyecto de calculadora, 100 dias con javascript

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let previousOperator;
 const screen = document.querySelector('.screen');
 
 function buttonClick(params) {
-    if (isNan(params)) {
+    if (isNaN(params)) {
         handleSymbol(params)
     } else {
-        handleSymbol(params)
+        handleNumber(params)
     }
     screen.innerText = buffer;
 }
@@ -33,10 +33,65 @@ function handleSymbol(symbol) {
                 buffer = '0';
             }
             else {
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1);
             }
+            break
+        case '+':
+            break
+        case '-':
+            break
+        case '×':
+            break
+        case '÷':
+            handleMath(symbol)
             break
         default:
             break;
     }
 }
+
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === 'x') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener("click", function (event) {
+        buttonClick(event.target.innerText);
+    })
+}
+
+init()
